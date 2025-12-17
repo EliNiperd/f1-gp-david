be016e7f5dda3e5349d096c0460150d5ef93366f
EliNiperd-Huawei <eli.rodriguez@niperd.mx>
2025-12-17 12:24:24 -0600

    Enable Next.js standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     domains: ["media.formula1.com"],
   },
-  /* config options here */
 };
 
 export default nextConfig;
